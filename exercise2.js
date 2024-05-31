e667b57c683a0cbe293c946ce8b41338eefca154
Johan Swan <jswannieman@gmail.com>
2024-05-31 15:39:46 +1200

    Update exercise2 JS Component

--- exercise2.js
+++ exercise2.js
@@ -1,12 +1,21 @@
 class Algebra {
-  constructor(_x, _y) {
-    (this.x = _x), (this.y = _y);
+  constructor(_a, _b, _c, _d, _e, _f, _g, _h) {
+    (this.a = _a),
+      (this.b = _b),
+      (this.c = _c),
+      (this.d = _d),
+      (this.e = _e),
+      (this.f = _f),
+      (this.g = _g),
+      (this.h = _h);
   }
   getTheSum() {
-    return `The sum of the 2 numbers are ${this.x + this.y}`;
+    return `The sum of the 2 numbers are ${
+      this.a + this.b + this.c + this.d + this.e + this.f + this.g + this.h
+    }`;
   }
 }
 
-let sum1 = new Algebra(17, 29);
+let sum1 = new Algebra(871, 934, 93, 118, 811, 457, 10, 200);
 
 console.log(sum1.getTheSum());
