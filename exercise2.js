1046174a04a2a29ca0a5684f0aeb01f6ebcf98c8
Johan Swan <jswannieman@gmail.com>
2024-06-02 14:06:05 +1200

    Update exercise2 JS Component

--- exercise2.js
+++ exercise2.js
@@ -10,15 +10,15 @@ class Algebra {
       (this.h = _h);
   }
   getTheSum() {
-    return `The sum of the 2 numbers are ${
+    return `The sum of the 8 numbers are ${
       this.a + this.b + this.c + this.d + this.e + this.f + this.g + this.h
     }`;
   }
 
   set otherValues(value) {
-    const rValue = [19, 27, 7, 80, 57, 22];
+    const rValue = [19, 27, 7, 80, 57, 22, 91, 14, 108, 77, 19, 33];
     this.a = value;
-    this.b = rValue[Math.floor(Math.random() * 6)];
+    this.b = rValue[Math.floor(Math.random() * 12)];
   }
 
   get getNewSum() {
@@ -30,6 +30,8 @@ let sum1 = new Algebra(871, 934, 93, 118, 811, 457, 10, 200);
 
 console.log(sum1.getTheSum());
 
-sum1.otherValues = 100;
+sum1.otherValues = 10;
+
+console.log(sum1.getTheSum());
 
 console.log(sum1.getNewSum);
